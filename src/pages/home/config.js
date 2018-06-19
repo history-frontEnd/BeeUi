4e2f6d7ec71d073c36f89e8c43b33232bbfecb2b
yyx <yyxcoder@163.com>
2018-06-19 16:10:08 +0800

    modal

--- src/pages/home/config.js
+++ src/pages/home/config.js
@@ -32,6 +32,12 @@ module.exports = {
           "name": "上传图片",
           "icon": "",
           "code": "https://s10.mogucdn.com/mlcdn/c45406/171107_1361j09d04c4426a5gd743i7kk5hg_480x480.jpg"
+        },
+        {
+          "id": "modal",
+          "name": "模态框",
+          "icon": "",
+          "code": "https://s10.mogucdn.com/mlcdn/c45406/171107_1361j09d04c4426a5gd743i7kk5hg_480x480.jpg"
         }
       ]
     },
