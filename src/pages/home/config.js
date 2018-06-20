c9fda98594660a3b598a2105ec0d086fa0d455cf
yyx <yyxcoder@163.com>
2018-06-20 10:43:02 +0800

    add timeline

--- src/pages/home/config.js
+++ src/pages/home/config.js
@@ -38,6 +38,12 @@ module.exports = {
           "name": "模态框",
           "icon": "",
           "code": "https://s10.mogucdn.com/mlcdn/c45406/171107_1361j09d04c4426a5gd743i7kk5hg_480x480.jpg"
+        },
+        {
+          "id": "timeline",
+          "name": "时间轴",
+          "icon": "",
+          "code": "https://s10.mogucdn.com/mlcdn/c45406/171107_1361j09d04c4426a5gd743i7kk5hg_480x480.jpg"
         }
       ]
     },
